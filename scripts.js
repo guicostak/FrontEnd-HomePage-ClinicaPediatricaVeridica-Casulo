50955d12c2e0335f543b60acd277926b0bf7dfbb
guicostak <103201399+guicostak@users.noreply.github.com>
2023-01-12 08:26:28 -0300

    Implementando menu mobile e animações

--- scripts.js
+++ scripts.js
@@ -1,3 +1,25 @@
+var openMenu = document.getElementById("open-menu")
+var menuMobile = document.querySelector(".menu-mobile")
+var closeMenu = document.getElementById("close-menu")
+
+
+function Menu(){
+  menuMobile.style.display = "block"
+  menuMobile.style.animation = "slideDown 1s alternate" 
+  closeMenu.style.animation = "none"
+  closeMenu.style.display = "block"
+}
+
+function fecharMenu(){
+  menuMobile.style.animation = "slideUp 1s alternate"
+    closeMenu.style.animation = "fadeOut 1s alternate"
+  
+  setTimeout(function () {
+    menuMobile.style.display = "none"
+  }, 1000) 
+}
+
+
 var count = 1
 document.getElementById("radio1").checked = true;
 
